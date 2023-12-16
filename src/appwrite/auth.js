ac629043b7ad82f151cfa2ea0d7948bcf4e10e44
shivam-batham <rsb7214@gmail.com>
2023-12-16 18:02:53 +0530

    login,createAccount , getCurrentUser service created

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -20,6 +20,7 @@ export class AuthService{
             const userAccount =  await this.account.create(ID.unique(),email,password,name);
             if(userAccount){
                 // call another method
+                return this.login({email,password})
 
             }else{
 
@@ -28,6 +29,24 @@ export class AuthService{
         }catch(error){
             throw error;
         }
+    }
+
+    async login({email,password}){
+        try{
+            return await this.account.createEmailSession(email,password);
+        }catch(error){
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        }catch(error){
+            throw error;
+        }
+
+        return null;
     }
 }
 

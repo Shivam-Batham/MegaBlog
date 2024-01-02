4f177739ff7d158f65090f2eef41cb0c91e2f6c9
shivam-batham <rsb7214@gmail.com>
2024-01-02 17:51:01 +0530

    bug fix9 PostFrom

--- src/conponents/Login.jsx
+++ src/conponents/Login.jsx
@@ -1,37 +1,31 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { login as authLogin } from "../store/authSlice";
-import { Button, Input, Logo } from "./index.js";
-import { useDispatch } from "react-redux";
-import authService from "../appwrite/auth";
-import { useForm } from "react-hook-form";
+import React, {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import { login as authLogin } from '../store/authSlice'
+import {Button, Input, Logo} from "./index"
+import {useDispatch} from "react-redux"
+import authService from "../appwrite/auth"
+import {useForm} from "react-hook-form"
 
 function Login() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const { register, handleSubmit } = useForm();
-  const [error, setError] = useState("");
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+    const [error, setError] = useState("")
 
-  const login = async (data) => {
-    setError('')
-    try{
-        // login karao data se
-        const session = await authService.login(data);
-        // agr login ho gaya h to currentuser ki detail nikalo
-        if(session){
-            const userData = await authService.getCurrentUser();
-            //agr detail aa gayi hai to store me state ko update kr do - useDispatch() se
-            if(userData){
-                // store login ko authlogin bola hai yaha
-                dispatch(authLogin(userData));
+    const login = async(data) => {
+        setError("")
+        try {
+            const session = await authService.login(data)
+            if (session) {
+                const userData = await authService.getCurrentUser()
+                if(userData) dispatch(authLogin(userData));
+                navigate("/")
             }
-            navigate('/')
+        } catch (error) {
+            setError(error.message)
         }
-
-    }catch(error){
-       setError(error.message)
     }
-  };
+
   return (
     <div
     className='flex items-center justify-center w-full'
@@ -53,46 +47,37 @@ function Login() {
                     </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        
-        <form onSubmit={handleSubmit(login)} className="mt-8">
+        <form onSubmit={handleSubmit(login)} className='mt-8'>
             <div className='space-y-5'>
-                
-                <Input 
-                    label='email: '
-                    placeholder = 'Enter your email'
-                    type='email'
-                    {...register("email",{
-                        required:true,
-                        validate:{
-                            matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                <Input
+                label="Email: "
+                placeholder="Enter your email"
+                type="email"
+                {...register("email", {
+                    required: true,
+                    validate: {
+                        matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
                         "Email address must be a valid address",
-                        }
-                    })
-
                     }
+                })}
                 />
-                {/* //password */}
-                <Input 
-                    label ='Password: '
-                    placeholder='enter password'
-                    type='password'
-                    {...register("password",{
-                        required:true
-                    })}
+                <Input
+                label="Password: "
+                type="password"
+                placeholder="Enter your password"
+                {...register("password", {
+                    required: true,
+                })}
                 />
-                
-                <Button 
+                <Button
                 type="submit"
-                className="w-full">
-                    Sign in
-                </Button>
+                className="w-full"
+                >Sign in</Button>
             </div>
-
         </form>
-
         </div>
     </div>
   )
 }
 
-export default Login;
+export default Login
